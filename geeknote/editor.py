37d72c1e63c3b9cb2ba5d47ac3c92bc99f6a9e64
James Quigley <jmquigley@outlook.com>
2014-10-20 00:28:58 -0500

    * Changed the markdown processor from markdown2 to Markdown so that table extensions could be added to the markdown syntax * Added Mac OS .DS_Store files to the ignore list

--- geeknote/editor.py
+++ geeknote/editor.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 import threading
 import hashlib
 import html2text as html2text
-import markdown2 as markdown
+import markdown as markdown
 import tools
 import out
 import re
@@ -87,7 +87,7 @@ class Editor(object):
             # add 2 space before new line in paragraph for creating br tags
             content = re.sub(r'([^\r\n])([\r\n])([^\r\n])', r'\1  \n\3', content)
             if format=='markdown':
-              contentHTML = markdown.markdown(content).encode("utf-8")
+              contentHTML = markdown.markdown(content, extensions=['markdown.extensions.extra']).encode("utf-8")
               # Non-Pretty HTML output
               contentHTML = str(BeautifulSoup(contentHTML, 'html.parser'))
             else:
