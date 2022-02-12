c5b1b8c02decd6e18e5b680561e94d02194dbcd7
Bernard Kerckenaere <bernieke@bernieke.com>
2022-02-12 12:53:41 +0100

    handle EDAMNotFoundException

--- geeknote/gnsync.py
+++ geeknote/gnsync.py
@@ -409,9 +409,10 @@ class GNSync:
                     )
                     logger.info("Saving image to {}".format(filename))
                     binaryHash = binascii.unhexlify(imageInfo["hash"])
-                    GeekNote(sleepOnRateLimit=self.sleep_on_ratelimit).saveMedia(
+                    if not GeekNote(sleepOnRateLimit=self.sleep_on_ratelimit).saveMedia(
                         note.guid, binaryHash, filename
-                    )
+                    ):
+                        logger.warning("Failed to save image {}".format(filename))
 
         content = Editor.ENMLtoText(note.content, self.imageOptions)
         path = os.path.join(self.path, escaped_title + self.extension)
