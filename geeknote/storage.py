b53a25538caf5501dc7cc24e4c6da50eccfdf49a
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2025-02-28 12:33:17 +0400

    Warning fix about sqlalchemy deprecated declarative_base()
    Full warning text was:
    
    geeknote/storage.py:17
      /home/vitaly/p/geeknote/geeknote/storage.py:17: MovedIn20Warning: The ``declarative_base()`` function is now available as sqlalchemy.orm.declarative_base(). (deprecated since: 2.0) (Background on SQLAlchemy 2.0 at: https://sqlalche.me/e/b8d9)
        Base = declarative_base()

--- geeknote/storage.py
+++ geeknote/storage.py
@@ -6,7 +6,6 @@ import pickle
 
 from sqlalchemy import *
 from sqlalchemy.orm import *
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import logging
